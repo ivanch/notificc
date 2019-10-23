23ffca96d0648ee5511dad98233bd202aa5bcff8
José Henrique Ivanchechen <joseivanchechen@outlook.com>
2019-10-23 10:41:02 -0300

    Fixes

---
+++ deploy_dockerhub.sh
@@ -0,0 +1,4 @@
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push ivanch/notificc-web:builder
+docker push ivanch/notificc-web:latest
+docker push ivanch/notificc-api:latest
